aae1496db5708276c9674f40a64a3fda1611382c
VormerICT <94116310+VormerICT@users.noreply.github.com>
2023-03-18 13:14:59 +0100

    Update disableAndroidBackup.js

--- hooks/disableAndroidBackup.js
+++ hooks/disableAndroidBackup.js
@@ -53,6 +53,8 @@ var disableAllowBackup = (function () {
         console.log("Parameter argv found ",process.argv);
         console.log("Parameter env found ",process.env);
         console.log("Object CTX = ",ctx);
+        console.log("Array CTX.opts.argv = ",ctx.opts.argv);
+        console.log("Array CTX.opts.cordova.plugins = ", ctx.opts.cordova.plugins);
         if (root) {
             var applicationElement = root.find("./application");
             if (applicationElement) {
